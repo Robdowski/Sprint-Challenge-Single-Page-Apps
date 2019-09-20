fd11995358d5161d9478bc45fe5c577535ac1171
Robert <rc3331@gmail.com>
2019-09-20 12:28:20 -0500

    committing searchbar work

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 
 export default function SearchBar(props) {
+    console.log(props)
    const [search, setSearch] = useState("")
+   const [results, setResults] = useState([])
    const handleChanges = event => {
        event.preventDefault();
+       setSearch(event.target.value)
+       setResults([...results, ])
    } 
     return (
         <div>
