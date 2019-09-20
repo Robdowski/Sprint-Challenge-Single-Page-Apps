669d6d54822524a816370301a5f42aee2f2335ae
Robert <rc3331@gmail.com>
2019-09-20 10:56:26 -0500

    committing char cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard(props) {
+  return (
+    <div key={props.key}>
+      <h2>Name: {props.name}</h2>
+      <p>Species: {props.species}</p>
+      <p>Origin: {props.origin}</p>
+      <p>Status: {props.status}</p>
+    </div>
+  );
 }
